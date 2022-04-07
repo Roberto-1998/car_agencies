a18bc2dc7989690c5c4dc5ef54da0622b47d1d94
Roberto <rcastellong98@gmail.com>
2022-04-07 21:23:42 +0200

    Creado el modelo de Usuario

---
+++ src/db/migrations/20220407191947-create-usuario.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Usuarios', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            nombre: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            apellidos: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            correo: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            edad: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            telefono: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Usuarios');
+    }
+};
---
+++ src/db/models/usuario.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Usuario extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Usuario.init({
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        apellidos: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        correo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        edad: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        telefono: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: 'usuarios',
+        sequelize,
+        modelName: 'Usuario',
+    });
+    return Usuario;
+};
